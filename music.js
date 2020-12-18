1b0c31d5b5e7f414f0d1c7aacea9e426ee750d70
Joel Mendivil <jmendivil171@west-mec.org>
2020-12-17 17:42:03 -0700

    Finished with the homepage and Joel's Page

--- music.js
+++ music.js
@@ -9,3 +9,30 @@ var scrollEventHandler = function()
 }
 
 window.addEventListener("scroll", scrollEventHandler, false);
+
+var imgArray = new Array();
+
+imgArray[0] = new Image();
+imgArray[0].src = 'Images/';
+
+function nextImage(element)
+{
+    var img = document.getElementById(element);
+
+    for(var i = 0;i<imgArray.length;i++)
+    {
+        if(imgArray[i] == img)
+        {
+            if(i == imgArray.length)
+            {
+                var j = 0;
+                document.getElementById(element).src = imgArray[j].src;
+                break;
+            }
+            else
+            var j = i + 1;
+            document.getElementById(element).src = imgArray[j].src;
+            break;
+        }
+    }   
+}
